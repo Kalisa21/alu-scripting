84967140fa8ec32473c4992d289dd77404bb3734
root <w.kalisa@alustudent.com>
2023-11-15 06:10:05 -0800

    regex

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env ruby
-input = "[Got]"
-
-# Extract 10-digit number from input using regular expression
-matched_number = input.scan(/\d{10}/).first
-
-# Output the matched number if found
-puts matched_number
+puts ARGV[0].scan(/^[0-9]{10}$/).join
